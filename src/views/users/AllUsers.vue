16fcca6fe10d5e9f0cdd675d5c256fc51edaf6b5
Evgenii Puliaev <puliaevevgenii@gmail.com>
2021-05-05 18:12:35 +0300

    SN-19 - created users page

---
+++ src/views/users/AllUsers.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <UserRow
+      v-for="item in users"
+      :key="item.name"
+      :user-data="item"
+      class="mx-auto my-3"
+    ></UserRow>
+  </v-container>
+</template>
+
+<script>
+import UserRow from "@/components/UserRow";
+import { FETCH_ALL_USERS_REQUEST } from "../../store/action-types";
+
+export default {
+  name: "AllUsers",
+  components: {
+    UserRow,
+  },
+  computed: {
+    users() {
+      return this.$store.getters.getAllUsers;
+    },
+  },
+  mounted() {
+    this.$store.dispatch(FETCH_ALL_USERS_REQUEST);
+  },
+};
+</script>
+
+<style scoped></style>
