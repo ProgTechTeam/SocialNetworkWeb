76f203f3beb11356b08fec87630428f0b85f268e
Evgenii Puliaev <puliaevevgenii@gmail.com>
2021-05-07 20:51:04 +0300

    msg

--- src/core/config/http.config.js
+++ src/core/config/http.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const HTTP = axios.create({
-  baseURL: "/api/",
+  baseURL: "http://social-network-dovecote.herokuapp.com/api/",
   timeout: 60 * 1000,
   responseType: "json",
 });
