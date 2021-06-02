e9c4a00851da29bbf3249a403a09ce1811a7f7c4
Burdyug Pavel <serhioburdug@gmail.com>
2021-06-02 11:14:53 +0300

    Likes and like cancellation

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -24,14 +24,53 @@
     <v-container fluid>
       {{ postData.payload }}
     </v-container>
+    <v-row class="mt-12">
+      <v-col class="ml-5" cols="12" sm="3">
+        <v-btn
+          icon
+          @click="likeAction"
+          :color="/*this.likesCount*/ isLiked ? 'pink' : ''"
+        >
+          <v-icon>mdi-heart</v-icon>
+          <span v-text="this.postData.likedUsers.length"></span>
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
 <script>
+import {
+  LIKE_POST_REQUEST,
+  CANCEL_LIKE_POST_REQUEST,
+} from "@/store/action-types";
+
 export default {
   name: "PostCard",
   props: {
     postData: {},
+  },
+  computed: {
+    isLiked() {
+      return this.postData.likedUsers.find(
+        (element) => element.id === this.$store.state.auth.currentUser.id
+      );
+    },
+  },
+  methods: {
+    likeAction: function () {
+      if (this.isLiked) {
+        this.cancelLike();
+      } else {
+        this.like();
+      }
+    },
+    like: function () {
+      this.$store.dispatch(LIKE_POST_REQUEST, this.postData.id);
+    },
+    cancelLike: function () {
+      this.$store.dispatch(CANCEL_LIKE_POST_REQUEST, this.postData.id);
+    },
   },
 };
 </script>
--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,12 +1,18 @@
 import {
+  CANCEL_LIKE_POST_ERROR,
+  CANCEL_LIKE_POST_REQUEST,
+  CANCEL_POST_LIKE_SUCCESS,
   CREATE_USER_POST_ERROR,
   CREATE_USER_POST_REQUEST,
   CREATE_USER_POST_SUCCESS,
   FETCH_USER_POSTS_ERROR,
   FETCH_USER_POSTS_REQUEST,
   FETCH_USER_POSTS_SUCCESS,
+  LIKE_POST_ERROR,
+  LIKE_POST_REQUEST,
+  LIKE_POST_SUCCESS,
 } from "../action-types";
-import { get, post } from "../../core/services/http.service";
+import { get, post, put } from "../../core/services/http.service";
 
 const state = () => ({
   posts: [],
@@ -29,6 +35,16 @@ const mutations = {
     state.posts.push(payload);
   },
   [CREATE_USER_POST_ERROR]: () => {},
+  [LIKE_POST_SUCCESS]: (state, payload) => {
+    const index = state.posts.findIndex((x) => x.id === payload.id);
+    state.posts[index].likedUsers = payload.likedUsers;
+  },
+  [LIKE_POST_ERROR]: () => {},
+  [CANCEL_POST_LIKE_SUCCESS]: (state, payload) => {
+    const index = state.posts.findIndex((x) => x.id === payload.id);
+    state.posts[index].likedUsers = payload.likedUsers;
+  },
+  [CANCEL_LIKE_POST_ERROR]: () => {},
 };
 
 const actions = {
@@ -50,6 +66,26 @@ const actions = {
       })
       .catch(() => {
         context.commit(CREATE_USER_POST_ERROR);
+      });
+  },
+  [LIKE_POST_REQUEST]: async (context, payload) => {
+    return await put(`users/${payload}/like`)
+      .then((response) => {
+        const payload = response && response.data;
+        context.commit(LIKE_POST_SUCCESS, payload);
+      })
+      .catch(() => {
+        context.commit(LIKE_POST_ERROR);
+      });
+  },
+  [CANCEL_LIKE_POST_REQUEST]: async (context, payload) => {
+    return await put(`users/${payload}/cancel-like`, payload)
+      .then((response) => {
+        const payload = response && response.data;
+        context.commit(CANCEL_POST_LIKE_SUCCESS, payload);
+      })
+      .catch(() => {
+        context.commit(CANCEL_LIKE_POST_ERROR);
       });
   },
 };
