dd614eb7e11657ff393e17ae5a9e31108e5a18f6
Evgenii Puliaev <puliaevevgenii@gmail.com>
2021-06-18 23:02:37 +0300

    SN-17 refactoring

---
+++ src/components/ChatRow.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-list-item
+    link
+    :to="{
+      name: 'ActiveChat',
+      params: {
+        id: item.id,
+        user: { name: item.name, avatar: item.avatar },
+      },
+    }"
+    class="mb-0"
+  >
+    <v-list-item-avatar>
+      <Avatar size="36px" :avatar="item.avatar" :rounded="false" />
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title class="d-flex justify-space-between">
+        <div>{{ item.name }}</div>
+        <div class="text-body-2 text--secondary">
+          {{ getFormattedTime(item.lastMessageTime) }}
+        </div>
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        {{ item.lastMessageAuthor }}: {{ item.lastMessagePayload }}
+      </v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+import Avatar from "@/components/Avatar";
+
+export default {
+  name: "ChatRow",
+  components: { Avatar },
+  props: {
+    item: {},
+  },
+  methods: {
+    getFormattedTime: function (timeRaw) {
+      const time = new Date(timeRaw);
+      return `${time.getHours()}:${time.getMinutes()}`;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/chats/Chats.vue
+++ src/views/chats/Chats.vue
@@ -6,34 +6,7 @@
           <v-list rounded subheader>
             <v-subheader inset>Чаты</v-subheader>
             <template v-for="(item, index) in chats">
-              <v-list-item
-                :key="item.id"
-                link
-                :to="{
-                  name: 'ActiveChat',
-                  params: {
-                    id: item.id,
-                    user: { name: item.name, avatar: item.avatar },
-                  },
-                }"
-                class="mb-0"
-              >
-                <v-list-item-avatar>
-                  <Avatar size="36px" :avatar="item.avatar" :rounded="false" />
-                </v-list-item-avatar>
-
-                <v-list-item-content>
-                  <v-list-item-title class="d-flex justify-space-between">
-                    <div>{{ item.name }}</div>
-                    <div class="text-body-2 text--secondary">
-                      {{ getFormattedTime(item.lastMessageTime) }}
-                    </div>
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    {{ item.lastMessageAuthor }}: {{ item.lastMessagePayload }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
+              <ChatRow :key="item.id" :item="item" />
               <v-divider
                 v-if="index !== chats.length - 1"
                 :key="'divider-' + item.id"
@@ -52,21 +25,15 @@
 </template>
 
 <script>
-import Avatar from "@/components/Avatar";
 import { FETCH_USER_CHATS_REQUEST } from "@/store/action-types";
+import ChatRow from "@/components/ChatRow";
 
 export default {
   name: "Chats",
-  components: { Avatar },
+  components: { ChatRow },
   computed: {
     chats() {
       return this.$store.getters.chatRows;
-    },
-  },
-  methods: {
-    getFormattedTime: function (timeRaw) {
-      const time = new Date(timeRaw);
-      return `${time.getHours()}:${time.getMinutes()}`;
     },
   },
   mounted() {
